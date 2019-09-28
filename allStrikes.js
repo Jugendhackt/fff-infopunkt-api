1a23b2eca0849feeb07c7138c8dab93d690e2c69
MagicMarvMan <marvin.kai@gmx.de>
2019-09-28 18:53:49 +0200

    Document get method for all strikes

--- allStrikes.js
+++ allStrikes.js
@@ -2,24 +2,32 @@
 const to = require('await-to-js').default
 
 const dynamoose = require("dynamoose")
+
+// set important aws credentials
 dynamoose.AWS.config.update({
     accessKeyId: process.env.AWS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET,
     region: process.env.AWS_LOCATION,
 })
+
+// check if we are using self-hosted dynamodb instance
 if(process.env.DB_LOCAL == "true") {
     console.log("Using local database with address \"" + process.env.DB_ADDRESS + "\"")
     dynamoose.local(process.env.DB_ADDRESS)
 } else console.log("Using cloud database")
 
+// import model; create if not existing
 const Strikes = require("./models/Strike")
 
 module.exports.allStrikes = async event => {
     console.log("Querying strikes...")
+    // query ALL elements in dynamodb
     const [err, items] = await to(Strikes.scan().exec())
     console.log("Query successful.")
     if(err) {
         console.log("Error.")
+
+        // return 500 if database query failed
         return {
             statusCode: 500,
             body: JSON.stringify(
@@ -31,6 +39,8 @@ module.exports.allStrikes = async event => {
         }
     }
     console.log("Success.")
+
+    // return all elements with success code (200)
     return {
         statusCode: 200,
         body: JSON.stringify(
@@ -42,6 +52,4 @@ module.exports.allStrikes = async event => {
         )
     }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
