709f2022888f7033341cab7d050a926705659c68
MagicMarvMan <marvin.kai@gmx.de>
2019-09-28 16:11:12 +0200

    Well the book was obviously wrong.

--- allStrikes.js
+++ allStrikes.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const dynamoose = require("dynamoose")
 dynamoose.AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -13,18 +11,28 @@ if(process.env.DB_LOCAL) {
 const Strikes = require("./models/Strike")
 
 module.exports.allStrikes = async event => {
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    const [err, items] = await to(Schools.scan().exec())
+    if(err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(
+                {
+                    code: "ERR_DATABASE",
+                    message: "Database query failed."
+                }
+            )
+        }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                code: "SUCCESS",
+                message: "",
+                items,
+            }
+        )
+    }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
